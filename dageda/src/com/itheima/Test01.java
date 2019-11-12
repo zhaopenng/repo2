0fa8af40371dfd6aff268fb1c0731427343c873b
zhaopeng <z18636139179@163.com>
2019-11-12 19:49:55 +0800

    123q2434

---
+++ dageda/src/com/itheima/Test01.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
+    }
+}
